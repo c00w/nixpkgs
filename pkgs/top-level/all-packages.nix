e60ac8ef1991a5841e6a2cf16a6014b20942c2ff
Colin L Rice <colin@daedrum.net>
2019-09-17 23:00:27 -0400

    Downgrade openshift to go1.12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19616,7 +19616,9 @@ in
 
   notion = callPackage ../applications/window-managers/notion { };
 
-  openshift = callPackage ../applications/networking/cluster/openshift { };
+  openshift = callPackage ../applications/networking/cluster/openshift {
+    buildGoPackage = buildGo112Package;
+  };
 
   oroborus = callPackage ../applications/window-managers/oroborus {};
 
